ca63e92891b77d1eed72a67e2324effe625e5d86
Shreedhar <shreedhar22@gmail.com>
2024-03-01 21:41:59 -0700

    add zk proof verfier function to establish the identity of the nominee

--- contracts/nominateAmbassador.sol
+++ contracts/nominateAmbassador.sol
@@ -28,26 +28,45 @@ contract AmbassadorNomination {
     // Event emitted when a citizen is nominated as an ambassador
     event AmbassadorNominated(address indexed nominee);
 
-
     // Function to make a commitment
     function makeCommitment(uint256 _r, uint256 _h) external {
         commitments[msg.sender] = Commitment(_r, _h);
         emit CommitmentMade(msg.sender, _r, _h);
     }
 
-
     // Function to nominate a citizen as an ambassador
-    function nominateAmbassador(address _nominee) external {
+    function nominateAmbassador(address _nominee, uint256 _x, uint256 _v) external {
         require(commitments[_nominee].r != 0, "Nominee has not made a commitment");
         require(nominations[_nominee].nominee == address(0), "Nominee has already been nominated");
 
         uint256 r = commitments[_nominee].r;
         uint256 h = commitments[_nominee].h;
 
-        // put in the verifyable ZKP algorithm here.
+        uint256 y = (G**_x) % P;
+        uint256 z = (r * (_v**h)) % P;
+
+        // Verify the ZKP
+        require(verifyZKP(_nominee, y, z), "Zero-knowledge proof verification failed");
 
         nominations[_nominee] = Nomination(h, _nominee);
         emit AmbassadorNominated(_nominee);
+    }
+
+    // Function to verify the ZKP
+    function verifyZKP(address _nominee, uint256 _y, uint256 _z) internal view returns (bool) {
+        uint256 r = commitments[_nominee].r;
+        uint256 h = commitments[_nominee].h;
+
+        uint256 v = 1;
+        uint256 vPowH = _y;
+        for (uint256 i = 0; i < MAX_ITERATIONS; i++) {
+            if ((G**v) % P == _y && (r * (_z**h)) % P == v) {
+                return true;
+            }
+            v = uint256(keccak256(abi.encodePacked(v, vPowH))) % P;
+            vPowH = (_y * vPowH) % P;
+        }
+        return false;
     }
 
 }
